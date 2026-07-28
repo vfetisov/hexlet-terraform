6980279a58bb4ca8fe1112920386d16c2fae5274
VF <vf@fetisov.one>
2026-07-28 09:01:55 +0300

    some commit

--- main.tf
+++ main.tf
@@ -28,29 +28,6 @@ provider "yandex" {
 // ================================================
 // Resources description
 
-resource "yandex_compute_instance" "default" {
-  name        = "vf-test-01"
-  platform_id = "standard-v1"
-  zone        = "ru-central1-a"
-  folder_id   = var.fldr_id
-
-  resources {
-    cores  = 2
-    memory = 4
-  }
-
-  boot_disk {
-    disk_id = yandex_compute_disk.default.id
-  }
-
-  network_interface {
-    subnet_id = "${yandex_vpc_subnet.default.id}"
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-  }
-}
 
 resource "yandex_vpc_network" "default" {
   folder_id = var.fldr_id
@@ -61,18 +38,6 @@ resource "yandex_vpc_subnet" "default" {
   network_id     = "${yandex_vpc_network.default.id}"
   v4_cidr_blocks = ["10.5.0.0/24"]
   folder_id      = var.fldr_id
-}
-
-resource "yandex_compute_disk" "default" {
-  name     = "disk-name"
-  type     = "network-ssd"
-  zone     = "ru-central1-a"
-  image_id = "fd83s8u085j3mq231ago" // идентификатор образа Ubuntu
-  folder_id = var.fldr_id
-
-  labels = {
-    environment = "test"
-  }
 }
 
 // Получение данных из облака
